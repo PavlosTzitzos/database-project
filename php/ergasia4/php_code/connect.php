d497438f3d233b53afe3425c58a58b46c27e0ca5
PavlosTzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2023-01-10 22:22:46 +0200

    Delete php/ergasia4 directory

--- php/ergasia4/php_code/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("dblab.nonrelevant.net","lab2223omada1","lab2223omada1","lab2223omada1_PROJECT_V2");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>

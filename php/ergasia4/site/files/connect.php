51b0756dd3a9f4ede2e73fd09d5a3f236d0e1408
pavlos tzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2023-01-12 20:21:41 +0200

    Add files via upload

---
+++ php/ergasia4/site/files/connect.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","lab2223omada1","lab2223omada1","lab2223omada1_PROJECT_V2");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>

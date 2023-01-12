0f0984c08eef674ee4f41ce108bf1c2fb9a35e8a
pavlos tzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2023-01-12 23:56:38 +0200

    Add files via upload

---
+++ php/ergasia4/site/files/JavaScript.js
@@ -0,0 +1,12 @@
+﻿VanillaTilt.init(document.querySelectorAll(".Card"), {
+    max: 25,
+    speed: 400,
+});
+for (let i = 0; i < sizes.length; i++) {
+    sises[i].addEventListener("click", () => {
+        for (let i = 0; i < sizes.length; i++) {
+            sizes[i].class.List.remove("active");
+        }
+    sizes[i].class.List.toggle("active");
+});
+}
